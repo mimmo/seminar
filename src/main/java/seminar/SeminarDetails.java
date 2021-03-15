59ca0195f528662fd0d4c3e1cc955bb92b9321e8
domenico.marini <domenico.marini@mfgroup.ch>
2021-03-15 07:56:44 +0100

    feat: two ways of generate html

--- src/main/java/seminar/SeminarDetails.java
+++ src/main/java/seminar/SeminarDetails.java
@@ -1,7 +1,5 @@
 package seminar;
 
-import org.jsoup.nodes.Element;
-
 public class SeminarDetails {
 	private Seminar _seminar;
 
@@ -9,47 +7,47 @@ public class SeminarDetails {
 		_seminar = seminar;
 	}
 
-	public String html() {
-		Element html = new Element("html");
-		Element head = new Element("head");
-		Element body = new Element("body");
-
-		html.appendChild(head);
-		head.appendElement("title").text(_seminar.getName().toString());
-		html.appendChild(body);
-		body.appendElement("div").text(_seminar.getName().toString() + ":");
-		Element courseInfo = body.appendElement("ul");
-		courseInfo.appendElement("li").text(_seminar.getDescritpion());
-		courseInfo.appendElement("li").text(_seminar.getLocation());
-		courseInfo.appendElement("li").text(String.valueOf(_seminar.getSeatsLeft()));
-		body.appendElement("div").text("partecipanti:");
-		Element participants = body.appendElement("ul");
-		for(String s: _seminar.getStudentsList()) {
-			participants.appendElement("li").text(s);
-		}
-
-		return html.toString();
-	}
-
 //	public String html() {
-//		String html = "<html><head><title>";
-//		html += _seminar.getName();
-//		html += "</title><body><div>";
-//		html += _seminar.getName();
-//		html += ":</div><ul><li>";
-//		html += _seminar.getDescritpion();
-//		html += "</li><li>";
-//		html += _seminar.getLocation();
-//		html += "</li><li>";
-//		html += String.valueOf(_seminar.getSeatsLeft());
-//		html += "</li></ul><div>partecipanti:</div><ul>";
-//		for(String s: _seminar.getStudentsList()) {
-//			html += "<li>" + s + "</li>";
-//		}
-//		html += "</ul>";
-//		html += "</body></html>";
+//		Element html = new Element("html");
+//		Element head = new Element("head");
+//		Element body = new Element("body");
 //
-//		return html;
+//		html.appendChild(head);
+//		head.appendElement("title").text(_seminar.getName().toString());
+//		html.appendChild(body);
+//		body.appendElement("div").text(_seminar.getName().toString() + ":");
+//		Element courseInfo = body.appendElement("ul");
+//		courseInfo.appendElement("li").text(_seminar.getDescritpion());
+//		courseInfo.appendElement("li").text(_seminar.getLocation());
+//		courseInfo.appendElement("li").text(String.valueOf(_seminar.getSeatsLeft()));
+//		body.appendElement("div").text("partecipanti:");
+//		Element participants = body.appendElement("ul");
+//		for(String s: _seminar.getStudentsList()) {
+//			participants.appendElement("li").text(s);
+//		}
+//
+//		return html.toString();
 //	}
+
+	public String html() {
+		String html = "<html><head><title>";
+		html += _seminar.getName();
+		html += "</title><body><div>";
+		html += _seminar.getName();
+		html += ":</div><ul><li>";
+		html += _seminar.getDescritpion();
+		html += "</li><li>";
+		html += _seminar.getLocation();
+		html += "</li><li>";
+		html += String.valueOf(_seminar.getSeatsLeft());
+		html += "</li></ul><div>partecipanti:</div><ul>";
+		for(String s: _seminar.getStudentsList()) {
+			html += "<li>" + s + "</li>";
+		}
+		html += "</ul>";
+		html += "</body></html>";
+
+		return html;
+	}
 
 }
